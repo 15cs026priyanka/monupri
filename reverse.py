7831460ec7324368e1d7b7f04430be5789faa441
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-27 15:42:20 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,5 @@
+a=int(input("enter the first string of the world")):
+b=int(input("enter the second string of the world")):
+str_reverse("given string"):
+for(i=0;i<n;i++):
+print("display the string")
